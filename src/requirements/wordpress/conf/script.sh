3ebecaa6e5d2a7af465d23c34b0c9e727f28d0c8
kaaaaakun_ <you@example.com>
2024-08-15 21:01:33 +0900

    fix: docker-compose.yml nginxのvolumeの設定

--- src/requirements/wordpress/conf/script.sh
+++ src/requirements/wordpress/conf/script.sh
@@ -9,6 +9,8 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar user create testuser testuser@example.com --user_pass=strongpassword --role=subscriber --allow-root
+
 
 php-fpm8.2 -F
 # -Fはphp-fpmをフォアグラウンドで実行するオプション
