bc3bc62cc8c40ad8293a0c7808cccc0629f7d65f
kaaaaakun_ <you@example.com>
2024-08-20 19:18:17 +0900

    fix: wp script.sh env

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -12,10 +12,10 @@ chmod +x wp-cli.phar
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
 
 # WordPressのインストール（未インストールの場合のみ）
-./wp-cli.phar core install --url="$URL" --title=inception --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASS" --admin_email="$ADMIN_EMAIL" --allow-root
+./wp-cli.phar core install --url="$URL" --title=inception --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASS" --admin_email="$ADMIN_MAIL" --allow-root
 
 # ユーザー作成
-./wp-cli.phar user create "$USER_NAME" "$USER_EMAIL" --user_pass="$USER_PASS" --role="$USER_ROLE" --allow-root
+./wp-cli.phar user create "$USER_NAME" "$USER_MAIL" --user_pass="$USER_PASS" --role="$USER_ROLE" --allow-root
 
 # php-fpmをフォアグラウンドで実行
 php-fpm8.2 -F
