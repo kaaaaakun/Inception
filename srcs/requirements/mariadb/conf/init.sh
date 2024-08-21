23941131b93a43bb01dde371fe3b71e34c75bf95
kaaaaakun_ <you@example.com>
2024-08-21 09:19:43 +0900

    fix: mariadbの変更

---
+++ srcs/requirements/mariadb/conf/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 環境変数を使って init.sql ファイルを生成
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME;" > init.sql
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> init.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
+
+cat init.sql
